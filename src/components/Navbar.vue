d7c95339aa70dd90497e78e113d4f0ecd4d08d31
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-07-01 14:28:17 +0200

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <nav id="NavBar" class="navbar navbar-expand-lg" style="background-color: rgb(0, 0, 0);" >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="" style="border-radius: 0%; height: 5rem;"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" color="white" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            <i class="bi bi-list"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <router-link class="text-white" to="/">Home</router-link>_
+              </li>
+              <li class="nav-item">
+                <router-link class="text-white" to="about">About</router-link>_
+              </li>
+              <li class="nav-item">
+                <router-link class="text-white" to="resume">Resume</router-link>_
+              </li>
+              <li class="nav-item">
+                <router-link class="text-white" to="projects">Projects</router-link>_
+              </li>
+              <li class="nav-item">
+                <router-link class="text-white" to="testimonials">Testimonials</router-link>_
+              </li>
+              <li class="nav-item">
+                <router-link class="text-white" to="contact">Contact</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,84 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section>
+  <div class="headings">
+    <h1 class="animate__animated animate__fadeInDown animate__delay-1s	1s display-5">Welcome to my <span> Portfolio </span></h1>
+    <h2 class="animate__animated animate__fadeIn animate__delay-2s	2s">My Name is Zia Khan</h2>
+    <h3 class="animate__animated animate__fadeInUp animate__delay-3s	3s">Aspiring <span> Web Developer </span></h3>
   </div>
+</section>
+  <section>
+  <div class="about">
+    <h1 class="about-title animate__animated animate__fadeInDown animate__delay-1s	1s">About meh...</h1>
+
+    </div>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          <p> Hey! I'm like a web developer wannabe, and I'm, like, super determined and stuff. When I found out about web development, I was like, "Wow, that's my thing!" I'm always up for a challenge, you know? Learning new coding stuff, trying out fancy frameworks, and keeping up with what's hot in the industry is my jam.
+
+            No matter what comes my way, I'm not gonna back down. I'm like a bulldozer, smashing through problems and learning from my mistakes. I pour my heart and soul into my work, making sure that the websites I build are not just okay but totally mind-blowing.
+            
+            Being a web developer is like my passion, man. I'm all about making cool stuff online and connecting people. It's like magic, dude! Even if things get tough, I won't give up. I'll find a way to make things work, no matter what.
+            
+            So, if you wanna work with me, be ready for some serious determination and awesomeness. Let's team up and make the internet a rad place together, dude!</p>
+        </div>
+      </div>
+    </div>
+  
+</section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
+
+<style scoped>
+  span {
+  
+    color: #ffffff;
+    text-align: center;
+    -webkit-animation: glow 1s ease-in-out infinite alternate;
+    -moz-animation: glow 1s ease-in-out infinite alternate;
+    animation: glow 1s ease-in-out infinite alternate;
+  }
+  
+  @keyframes glow {
+    from {
+      text-shadow: 0 0 10px #06a600, 0 0 20px #48ff00, 0 0 30px;
+    }
+    to {
+      text-shadow: 0 0 20px #00a308, 0 0 30px #6bff4d, 0 0 40px #a0ff4d;
+    }
+  }
+
+  .headings{
+    margin-top: 14rem;
+  }
+
+  .about{
+    padding-top: 20rem;
+  }
+
+  p{
+    font-size: 1.3rem;
+  }
+
+  .about-title{
+    font-size: 3.5rem;
+    padding-bottom: 4rem;
+  }
+
+  .row{
+    padding-inline: 15rem;
+  }
+
+  .col{
+    gap: 7rem;
+  }
+
+</style>

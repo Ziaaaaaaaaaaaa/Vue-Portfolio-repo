9bd993a4bac1981385b0994c3d9ac33ab8f443e6
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-07-06 13:35:32 +0200

    commit

--- src/store/index.js
+++ src/store/index.js
@@ -26,20 +26,38 @@ export default createStore({
   },
   actions: {
     async fetchData({commit}){
-      const fetchedData = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
+      try {
+        const fetchedData = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
       commit('setState', fetchedData.data.projects)
+      } catch (error) {
+        alert(error);
+      }
+      
     },
     async fetchDataEducation({commit}){
-      const fetchDataEducation = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
-      commit('setStateEducation', fetchDataEducation.data.education)
+      try {
+        const fetchDataEducation = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
+        commit('setStateEducation', fetchDataEducation.data.education)
+      } catch (error) {
+        alert(error);
+      }
+
     },
     async fetchDataSkills({commit}){
-      const fetchDataSkills = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
-      commit('setStateSkills', fetchDataSkills.data.skills)
+      try {
+        const fetchDataSkills = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
+        commit('setStateSkills', fetchDataSkills.data.skills)
+      } catch (error) {
+        alert(error);
+      }
     },
     async fetchDataTestimonials({commit}){
-      const fetchDataTestimonials = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
-      commit('setStateTestimonials', fetchDataTestimonials.data.testimonials)
+      try {
+        const fetchDataTestimonials = await axios.get('https://ziaaaaaaaaaaaa.github.io/Json-Portfolio-Server/portfolio.json')
+        commit('setStateTestimonials', fetchDataTestimonials.data.testimonials)
+      } catch (error) {
+        alert(error);
+      }
     }
   },
   modules: {
